eb55081595250249655ad76903b3db7c8ab20b2d
delaemon <takeshi.music.5@gmail.com>
2016-02-11 11:47:39 +0900

    Third day

---
+++ onchange.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <uv.h>
+
+char *command;
+uv_loop_t *loop;
+
+void run_command(uv_fs_event_t *handle, const char *filename, int events, int status) {
+    char path[1024];
+    size_t size = 1023;
+    uv_fs_event_getpath(handle, path, &size);
+    path[size] = '\0';
+
+    fprintf(stderr, "Change detected in %s ",path);
+    if (events & UV_RENAME) {
+        fprintf(stderr, "renamed");
+    }
+    if (events & UV_CHANGE) {
+        fprintf(stderr, "changed");
+    }
+
+    fprintf(stderr, " %s \n", filename ? filename : "");
+    system(command);
+}
+
+int main(int argc, char **argv) {
+    if(argc <= 2) {
+        fprintf(stderr, "Usage: %s <command> <file1> [file2...]\n", argv[0]);
+        return 1;
+    }
+
+    loop = uv_default_loop();
+    command = argv[1];
+
+    while (argc-- > 2) {
+        fprintf(stderr, "Adding watch on %s \n", argv[argc]);
+        uv_fs_event_t *fs_event_req = malloc(sizeof(uv_fs_event_t));
+        uv_fs_event_init(loop, fs_event_req);
+        uv_fs_event_start(fs_event_req, run_command, argv[argc], UV_FS_EVENT_RECURSIVE);
+    }
+
+    return uv_run(loop, UV_RUN_DEFAULT);
+}
