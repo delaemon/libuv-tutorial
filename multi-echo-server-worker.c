8f6e82f0a3c2991bc2f2756d157264dd1ee8c80d
delaemon <takeshi.music.5@gmail.com>
2016-03-10 08:47:43 +0900

    Add multi echo

---
+++ multi-echo-server-worker.c
@@ -0,0 +1,74 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <uv.h>
+
+uv_loop_t *loop;
+uv_pipe_t queue;
+
+void alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf) {
+    buf->base = malloc(suggested_size);
+    buf->len = suggested_size;
+}
+
+void echo_write(uv_write_t *req, int status) {
+    if (status) {
+        fprintf(stderr, "Write error %s\n", uv_err_name(status));
+    }
+    free(req);
+}
+
+void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
+    if (nread < 0) {
+        if (nread != UV_EOF) {
+            fprintf(stderr, "Read error %s\n", uv_err_name(nread));
+        }
+        uv_close((uv_handle_t*) client, NULL);
+        return;
+    }
+
+    uv_write_t *req = (uv_write_t *)malloc(sizeof(uv_write_t));
+    uv_buf_t wrbuf = uv_buf_init(buf->base, nread);
+    uv_write(req, client, &wrbuf, 1, echo_write);
+    free(buf->base);
+}
+
+void on_new_connection(uv_stream_t *q, ssize_t nread, uv_buf_t *buf) {
+    if (nread < 0) {
+        if (nread != UV_EOF) {
+            fprintf(stderr, "Read error %s\n", uv_err_name(nread));
+        }
+        uv_close((uv_handle_t*)q, NULL);
+        return;
+    }
+
+    uv_pipe_t *pipe = (uv_pipe_t*)q;
+    if (!uv_pipe_pending_count(pipe)) {
+        fprintf(stderr, "No pending count\n");
+        return;
+    }
+
+    uv_handle_type pending = uv_pipe_pending_type(pipe);
+    assert(pending == UV_TCP);
+
+    uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
+    uv_tcp_init(loop, client);
+    if (uv_accept(q, (uv_stream_t*) client) == 0) {
+        uv_os_fd_t fd;
+        uv_fileno((const uv_handle_t*) client, &fd);
+        fprintf(stderr, "Worker %d: Accepted fd %d\n", getpid(), fd);
+        uv_read_start((uv_stream_t*) client, alloc_buffer, echo_read);
+    } else {
+        uv_close((uv_handle_t*) client, NULL);
+    }
+}
+
+int main() {
+    loop = uv_default_loop();
+    uv_pipe_init(loop, &queue, 1);
+    uv_pipe_open(&queue, 0);
+    uv_read_start((uv_stream_t*)&queue, alloc_buffer, echo_read);
+    return uv_run(loop, UV_RUN_DEFAULT);
+}
